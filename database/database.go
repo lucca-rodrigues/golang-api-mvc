18ebaf62e8f0f9d4ced3da88132ad5806f6fcc3f
Lucas <lucas@MacBook-Air-de-Lucas.local>
2022-09-29 12:49:36 -0300

    create database connection

--- database/database.go
+++ database/database.go
@@ -1,1 +1,50 @@
 package database
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/luccarodrigues/golang-api-mvc/database/migrations"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func startDB(){
+	connection := "host=localhost port=5432 user=admin dbname=api-golang sslmode=disable password=api-golang"
+
+	database, err := gorm.Open(postgres.Open(connection), &gorm.Config{})
+
+	if err != nil {
+		fmt.Println("Could not connect to the Postgres Database")
+		log.Fatal("Error: ", err)
+	}
+
+	db = database
+	config, _ := db.DB()
+	config.SetMaxIdleConns(10)
+	config.SetMaxOpenConns(100)
+	config.SetConnMaxLifetime(time.Hour)
+
+	migrations.RunMigrations(db)
+}
+
+func CloseConn() error {
+	config, err := db.DB()
+	if err != nil {
+		return err
+	}
+
+	err = config.Close()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func GetDatabase() *gorm.DB {
+	return db
+}
